ae598732425cd6a11541b868cc7c52b28773d6f5
nega <nega@icecube.umd.edu>
2014-12-01 16:59:05 -0500

    fix test data to use $I3_TESTDATA
    this could also be done w/ defines in CMakeLists.txt

--- public/nuSQuIDS/global.h
+++ public/nuSQuIDS/global.h
@@ -2,12 +2,15 @@
 #ifndef __GLOBAL_H
 #define __GLOBAL_H
 
+#include <cstdlib>
+#include <string>
 
 #define XSECTION_LOCATION "/Users/carguelles/Workspace/SQuIDS/git_version/nuSQuIDS/data/xsections/"
 #define SUN_MODEL_LOCATION  "/Users/carguelles/Workspace/SQuIDS/git_version/nuSQuIDS/data/astro/bs05_agsop.dat"
 #define SUN_MODEL_NELECTRON_LOCATION "/Users/carguelles/Workspace/SQuIDS/git_version/nuSQuIDS/data/astro/nele_bs05op.dat"
-#define EARTH_MODEL_LOCATION "/Users/carguelles/Workspace/SQuIDS/git_version/nuSQuIDS/data/astro/EARTH_MODEL_PREM.dat"
-
+// #define EARTH_MODEL_LOCATION "resources/data/astro/EARTH_MODEL_PREM.dat"
+const std::string i3_testdata = getenv("I3_TESTDATA");
+const std::string EARTH_MODEL_LOCATION = i3_testdata + "/nuSQuIDS/astro/EARTH_MODEL_PREM.dat";
 
 #endif
 
