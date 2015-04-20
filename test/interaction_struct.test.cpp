2bd7ff711a0f218a9a9f11d086c29401b17b646a
Carlos Arguelles <arguelles@wisc.edu>
2015-04-19 20:21:50 -0500

    update test

--- test/interaction_struct.test.cpp
+++ test/interaction_struct.test.cpp
@@ -11,10 +11,18 @@ int main(){
   auto e_range = nus1.GetERange();
   auto int_struct = nus1.GetInteractionStructure();
 
-  marray<double,1> costh_range {{10},{-1,-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,}};
+  marray<double,1> costh_range {{10},{-1,-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1}};
   nuSQUIDSAtm<> nusatm(costh_range,e_range,3,neutrino,true,nullptr);
 
   if(*(nusatm.GetInteractionStructure)() != (*int_struct)){
+    std::cout << "Fail" << std::endl;
+  }
+
+  nusatm.WriteStateHDF5("atm_test.hdf5");
+
+  nuSQUIDSAtm<> nusatm_2("atm_test.hdf5");
+
+  if(*(nusatm_2.GetInteractionStructure)() != (*int_struct)){
     std::cout << "Fail" << std::endl;
   }
 
