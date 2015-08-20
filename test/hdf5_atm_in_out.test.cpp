dc6a4547cf60243aaca89ccab255fe767503e7a8
cnweaver <cnweaver@gmail.com>
2015-08-20 10:03:56 -0600

    When testing differences, absolute values help detect errors in both directions

--- test/hdf5_atm_in_out.test.cpp
+++ test/hdf5_atm_in_out.test.cpp
@@ -59,7 +59,7 @@ int main(){
   for (nuSQUIDS &nsq : nus.GetnuSQuIDS()){
     nuSQUIDS &nsq_read = nus_read.GetnuSQuIDS(counter);
     double squids_time_diff = nsq.Get_t() - nsq_read.Get_t();
-    if (squids_time_diff > 1.0e-15)
+    if (std::abs(squids_time_diff) > 1.0e-15)
       std::cout << nsq.Get_t() << " " << nsq_read.Get_t() << std::endl;
     counter++;
   }
@@ -68,7 +68,7 @@ int main(){
   for (nuSQUIDS &nsq : nus.GetnuSQuIDS()){
     nuSQUIDS &nsq_read = nus_read.GetnuSQuIDS(counter);
     double squids_time_initial_diff = nsq.Get_t_initial() - nsq_read.Get_t_initial();
-    if (squids_time_initial_diff > 1.0e-15)
+    if (std::abs(squids_time_initial_diff) > 1.0e-15)
       std::cout << nsq.Get_t_initial() << " " << nsq_read.Get_t_initial() << std::endl;
     counter++;
   }
@@ -93,9 +93,9 @@ int main(){
     for ( int j = 0; j < i ; j ++){
       double diff_th = nus.Get_MixingAngle(j,i) - nus_read.Get_MixingAngle(j,i);
       double diff_cp = nus.Get_CPPhase(j,i) - nus_read.Get_CPPhase(j,i);
-      if ( diff_th > 1.0e-15 )
+      if ( std::abs(diff_th) > 1.0e-15 )
         std::cout << "MA " << nus.Get_MixingAngle(j,i) << " " << nus_read.Get_MixingAngle(j,i) << std::endl;
-      if ( diff_cp > 1.0e-15 )
+      if ( std::abs(diff_cp) > 1.0e-15 )
         std::cout << "CP " << nus.Get_CPPhase(j,i) << " " << nus_read.Get_CPPhase(j,i) << std::endl;
     }
   }
@@ -103,7 +103,7 @@ int main(){
   // check that square mass differences are the same
   for ( int i = 1; i < nus.GetNumNeu() ; i++){
     double diff_dmsq = nus.Get_SquareMassDifference(i) - nus_read.Get_SquareMassDifference(i);
-    if (diff_dmsq > 1.0e-15)
+    if (std::abs(diff_dmsq) > 1.0e-15)
       std::cout << "DMSQ "<< nus.Get_SquareMassDifference(i) << " " << nus_read.Get_SquareMassDifference(i) << std::endl;
   }
 
@@ -114,12 +114,12 @@ int main(){
     for ( int i = 0 ; i < nus_read.GetNumNeu(); i++){
       squids::SU_vector fproj_diff = nsq.GetFlavorProj(i) - nsq_read.GetFlavorProj(i);
       for ( double component : fproj_diff.GetComponents()){
-        if (component > 1.0e-15)
+        if (std::abs(component) > 1.0e-15)
           std::cout << "FP" << component << std::endl;
       }
       squids::SU_vector mproj_diff = nsq.GetMassProj(i) - nsq_read.GetMassProj(i);
       for ( double component : mproj_diff.GetComponents()){
-        if (component > 1.0e-15)
+        if (std::abs(component) > 1.0e-15)
           std::cout << "MP" << component << std::endl;
       }
     }
@@ -149,7 +149,7 @@ int main(){
     nuSQUIDS &nsq_read = nus_read.GetnuSQuIDS(counter);
     for ( int i = 0 ; i < nsq.GetBody()->GetBodyParams().size(); i++){
       double body_params_diff = nsq.GetBody()->GetBodyParams()[i] - nsq_read.GetBody()->GetBodyParams()[i];
-      if (body_params_diff > 1.0e-15)
+      if (std::abs(body_params_diff) > 1.0e-15)
         std::cout << "BP " << body_params_diff << std::endl;
     }
     counter++;
@@ -169,7 +169,7 @@ int main(){
     nuSQUIDS &nsq_read = nus_read.GetnuSQuIDS(counter);
     for ( int i = 0 ; i < nsq.GetTrack()->GetTrackParams().size(); i++){
       double track_params_diff = nsq.GetTrack()->GetTrackParams()[i] - nsq_read.GetTrack()->GetTrackParams()[i];
-      if (track_params_diff > 1.0e-15)
+      if (std::abs(track_params_diff) > 1.0e-15)
         std::cout << "TP " << track_params_diff << std::endl;
     }
     counter++;
@@ -179,7 +179,7 @@ int main(){
   for (nuSQUIDS &nsq : nus.GetnuSQuIDS()){
     nuSQUIDS &nsq_read = nus_read.GetnuSQuIDS(counter);
     double diff_x_current = nsq.GetTrack()->GetX() - nsq_read.GetTrack()->GetX();
-    if ( diff_x_current > 1.0e-15 )
+    if ( std::abs(diff_x_current) > 1.0e-15 )
       std::cout << "XC " << nsq.GetTrack()->GetX() << " " << nsq_read.GetTrack()->GetX() << std::endl;
     counter++;
   }
@@ -188,7 +188,7 @@ int main(){
   for (nuSQUIDS &nsq : nus.GetnuSQuIDS()){
     nuSQUIDS &nsq_read = nus_read.GetnuSQuIDS(counter);
     double diff_x_initial = nsq.GetTrack()->GetInitialX() - nsq_read.GetTrack()->GetInitialX();
-    if ( diff_x_initial > 1.0e-15 )
+    if ( std::abs(diff_x_initial) > 1.0e-15 )
       std::cout << "XI " << nsq.GetTrack()->GetInitialX() << " " << nsq_read.GetTrack()->GetInitialX() << std::endl;
     counter++;
   }
@@ -197,7 +197,7 @@ int main(){
   for (nuSQUIDS &nsq : nus.GetnuSQuIDS()){
     nuSQUIDS &nsq_read = nus_read.GetnuSQuIDS(counter);
     double diff_x_final = nsq.GetTrack()->GetFinalX() - nsq_read.GetTrack()->GetFinalX();
-    if ( diff_x_final > 1.0e-15 )
+    if ( std::abs(diff_x_final) > 1.0e-15 )
       std::cout << "XF " << nsq.GetTrack()->GetFinalX() << " " << nsq_read.GetTrack()->GetFinalX() << std::endl;
     counter++;
   }
@@ -209,7 +209,7 @@ int main(){
     for ( int i = 0 ; i < nus_read.GetNumE(); i++){
       squids::SU_vector hamiltonian_diff = nsq.GetHamiltonian(i) - nsq_read.GetHamiltonian(i);
       for (double component : hamiltonian_diff.GetComponents()){
-        if (component > 1.0e-15)
+        if (std::abs(component) > 1.0e-15)
           std::cout << "HC" << component << std::endl;
       }
     }
@@ -223,7 +223,7 @@ int main(){
     for ( int i = 0 ; i < nus_read.GetNumE(); i++){
       squids::SU_vector state_diff = nsq.GetState(i) - nsq_read.GetState(i);
       for (double component : state_diff.GetComponents()){
-        if (component > 1.0e-15)
+        if (std::abs(component) > 1.0e-15)
           std::cout << "SC" << component << std::endl;
       }
     }
@@ -237,7 +237,7 @@ int main(){
     for ( int i = 0 ; i < nsq_read.GetNumE(); i++){
         for ( int k = 0; k < nsq_read.GetNumNeu(); k++){
           double dif = fabs(nsq.EvalFlavorAtNode(k,i) - nsq_read.EvalFlavorAtNode(k,i));
-          if (dif > 1.0e-15)
+          if (std::abs(dif) > 1.0e-15)
             std::cout << "DIF" << i << " " << k << " " << dif << std::endl;
         }
     }
@@ -254,7 +254,7 @@ int main(){
         double enu = enu_range[ei]+0.1;
 
         double dif = fabs(nus.EvalFlavor(k,costh,enu) - nus_read.EvalFlavor(k,costh,enu));
-        if (dif > 1.0e-15)
+        if (std::abs(dif) > 1.0e-15)
           std::cout << "DIF" << costh << " " << enu << " " << k << " " << dif << std::endl;
       }
     }
