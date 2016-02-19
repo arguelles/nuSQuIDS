8fe464c7dc46ae3043bc47d0bec8824332c42ae2
Carlos Arguelles <carguelles@magi.local>
2016-02-19 16:10:21 -0500

    improved cross section consistency test

--- test/cross_section_consistency.test.cpp
+++ test/cross_section_consistency.test.cpp
@@ -33,16 +33,16 @@ int main(){
         double total_dNdECC = 0;
         double begincap_dNdECC = energies[0]*is->dNdE_CC[rho][flv][ei][0];
         for(unsigned int ef = 0; ef < ei; ef++){
-          total_dNdECC += is->dNdE_CC[rho][flv][ei][ef]*(energies[ef+1]-energies[ef]);
+          total_dNdECC += (is->dNdE_CC[rho][flv][ei][ef+1]+is->dNdE_CC[rho][flv][ei][ef])*(energies[ef+1]-energies[ef])/2.;
         }
-        if(std::abs(total_dNdECC + endcap_dNdECC - 1.0) > 1.0e-2)
+        if(std::abs(total_dNdECC + begincap_dNdECC - 1.0) > 1.0e-2)
           std::cout << "Error: CC cross sectiosn are not unitarity: "<< ei << " " << total_dNdECC + begincap_dNdECC << std::endl;
         double total_dNdENC = 0;
         double begincap_dNdENC = energies[0]*is->dNdE_NC[rho][flv][ei][0];
         for(unsigned int ef = 0; ef < ei; ef++){
           total_dNdENC += is->dNdE_NC[rho][flv][ei][ef]*(energies[ef+1]-energies[ef]);
         }
-        if(std::abs(total_dNdENC + endcap_dNdENC - 1.0) > 1.0e-2)
+        if(std::abs(total_dNdENC + begincap_dNdENC - 1.0) > 1.0e-2)
           std::cout << "Error: NC cross sectiosn are not unitarity: "<< ei << " " << total_dNdENC + begincap_dNdENC << std::endl;
       }
     }
