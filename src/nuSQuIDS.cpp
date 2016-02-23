b7d4a4c3d8e3d35ff249d42c8517373c99538766
cnweaver <cnweaver@gmail.com>
2016-02-22 22:37:14 -0700

    Don't demand non-zero cross sections for sterile flavors

--- src/nuSQuIDS.cpp
+++ src/nuSQuIDS.cpp
@@ -639,7 +639,8 @@ void nuSQUIDS::InitializeInteractions(){
     }
   
     auto validateCrossSection=[this](double value, double unit, const char* interaction, bool singleDiff, double neuE, double leptE, int flavor){
-      if(value<=0.0 || std::isnan(value)){
+      //all active neutrino types should have sensible cross sections
+      if(flavor<=3 && (value<=0.0 || std::isinf(value) || std::isnan(value))){
         std::ostringstream ss;
         ss << "Invalid " << interaction << (singleDiff ? " singly-differential" : " total")
         << " cross section value: " << value/unit << '\n';
