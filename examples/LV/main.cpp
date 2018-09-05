4216e0de8e5795be46681431d6e34bc804d85772
Carlos Arguelles <carguelles@nimbostratus.local>
2018-09-05 12:52:17 +0200

    small fix in LV examples

--- examples/LV/main.cpp
+++ examples/LV/main.cpp
@@ -44,59 +44,56 @@ int main()
   //number of neutrinos
   unsigned int numneu=3;
   //Value for the epsilon mutau
-  double eps_mutau=1.0e-2;
+  double re_c_mutau =1.0e-20*units.eV;
   //minimum and maximum energy
   double Emin=1.e1*units.GeV;
   double Emax=1.e3*units.GeV;
-  //Declaration of the nusquids NSI object, for more details about how to construct an object
-  //like this look at the file NSI.h
-  //We declare two objects "nus" with NSI value of eps_mutau and nus_zero with 0.0
-  nuSQUIDSLV nus(logspace(Emin,Emax,200),numneu,antineutrino,false);
-  nuSQUIDSLV nus_zero(logspace(Emin,Emax,200),numneu,antineutrino,false);
+  //Declaration of the nusquids LV object, for more details about how to construct an object
+  //like this look at the file LV.h
+  nuSQUIDSLV nus_lv(logspace(Emin,Emax,200),numneu,antineutrino,false);
+  nuSQUIDSLV nus_std(logspace(Emin,Emax,200),numneu,antineutrino,false);
 
   gsl_complex zero {0.0*units.eV,0.0*units.eV};
   LVParameters null {zero, zero};
-  nus.Set_LV_OpMatrix(null);
+  nus_std.Set_LV_OpMatrix(null);
 
-  gsl_complex cmutau {1.e-20*units.eV,0.0*units.eV}
+  gsl_complex cmutau {re_c_mutau*units.eV,0.0*units.eV};
   LVParameters non_null_lv{ zero, cmutau };
-  nus.Set_LV_OpMatrix(non_null_lv);
+  nus_lv.Set_LV_OpMatrix(non_null_lv);
 
   //Setting up the propagation medium and track. For the track the number provided is the baseline.
   std::shared_ptr<Earth> earth = std::make_shared<Earth>();
   std::shared_ptr<Earth::Track> track = std::make_shared<Earth::Track>(10000.*units.km);
 
-  nus.Set_Body(earth);
-  nus.Set_Track(track);
+  nus_std.Set_Body(earth);
+  nus_std.Set_Track(track);
 
-  nus_zero.Set_Body(earth);
-  nus_zero.Set_Track(track);
+  nus_lv.Set_Body(earth);
+  nus_lv.Set_Track(track);
 
   //Set mixing angles and masses
-  nus.Set_MixingAngle(0,1,0.563942);
-  nus.Set_MixingAngle(0,2,0.154085);
-  nus.Set_MixingAngle(1,2,0.785398);
+  nus_std.Set_MixingAngle(0,1,0.563942);
+  nus_std.Set_MixingAngle(0,2,0.154085);
+  nus_std.Set_MixingAngle(1,2,0.785398);
 
-  nus.Set_SquareMassDifference(1,7.65e-05);
-  nus.Set_SquareMassDifference(2,0.00247);
+  nus_std.Set_SquareMassDifference(1,7.65e-05);
+  nus_std.Set_SquareMassDifference(2,0.00247);
 
-  nus_zero.Set_MixingAngle(0,1,0.563942);
-  nus_zero.Set_MixingAngle(0,2,0.154085);
-  nus_zero.Set_MixingAngle(1,2,0.785398);
+  nus_lv.Set_MixingAngle(0,1,0.563942);
+  nus_lv.Set_MixingAngle(0,2,0.154085);
+  nus_lv.Set_MixingAngle(1,2,0.785398);
 
-  nus_zero.Set_SquareMassDifference(1,7.65e-05);
-  nus_zero.Set_SquareMassDifference(2,0.00247);
+  nus_lv.Set_SquareMassDifference(1,7.65e-05);
+  nus_lv.Set_SquareMassDifference(2,0.00247);
 
   //Setup integration settings
-  nus.Set_h_max( 200.0*units.km );
-  nus.Set_rel_error(1.0e-15);
-  nus.Set_abs_error(1.0e-15);
+  nus_std.Set_rel_error(1.0e-15);
+  nus_std.Set_abs_error(1.0e-15);
 
-  nus_zero.Set_h_max( 200.0*units.km );
-  nus_zero.Set_rel_error(1.0e-15);
-  nus_zero.Set_abs_error(1.0e-15);
+  nus_lv.Set_rel_error(1.0e-15);
+  nus_lv.Set_abs_error(1.0e-15);
 
-  marray<double,1> E_range = nus.GetERange();
+  marray<double,1> E_range = nus_std.GetERange();
 
   //Construct the initial state
   marray<double,2> inistate({200,numneu});
@@ -109,18 +106,18 @@ int main()
   }
 
   // set the initial state
-  nus.Set_initial_state(inistate,flavor);
-  nus_zero.Set_initial_state(inistate,flavor);
+  nus_std.Set_initial_state(inistate,flavor);
+  nus_lv.Set_initial_state(inistate,flavor);
 
   //Here we propagate the two neutrino fluxes, one with NSI and the other without NSI
-  nus.Set_ProgressBar(true);
-  nus_zero.Set_ProgressBar(true);
-  std::cout <<"propagating the standard case, non-NSI ..." << std::endl;
-  nus.EvolveState();
+  nus_lv.Set_ProgressBar(true);
+  nus_std.Set_ProgressBar(true);
+  std::cout <<"propagating the standard case, non-LV..." << std::endl;
+  nus_std.EvolveState();
   std::cout << std::endl;
 
-  std::cout <<"propagating the NSI case with epsilon_mutau=" << eps_mutau << " ..." << std::endl;
-  nus_zero.EvolveState();
+  std::cout <<"propagating the NSI case with c_mutau=" << re_c_mutau << " ..." << std::endl;
+  nus_lv.EvolveState();
   std::cout << std::endl;
 
   int Nen =1000;
@@ -134,7 +131,7 @@ int main()
     double E=pow(10.0,lE);
     file << lE << " " << E << " ";
     for(int fl=0; fl<numneu; fl++){
-      file << " " <<  nus.EvalFlavor(fl, E) << " " <<  nus_zero.EvalFlavor(fl, E);
+      file << " " <<  nus_std.EvalFlavor(fl, E) << " " <<  nus_lv.EvalFlavor(fl, E);
     }
     file << std::endl;
   }
