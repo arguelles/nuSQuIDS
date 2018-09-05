abbef890f413dd1fd1ac68320e83fa4ed057606f
Carlos Arguelles <carguelles@nimbostratus.local>
2018-09-05 17:34:55 +0200

    new cross section example

--- examples/Xsections/exCross.cpp
+++ examples/Xsections/exCross.cpp
@@ -1,29 +1,18 @@
 #include "exCross.h"
 
-
 namespace nusquids{
-
-
-  double NeutrinoDISCrossSectionsFromTablesExtended::TotalCrossSection(double Enu, NeutrinoFlavor flavor,
-								       NeutrinoType neutype, Current current) const{
-    // we assume that sterile neutrinos are trully sterile
-    if (not (flavor == electron or flavor == muon or flavor == tau))
-      return 0.0;
-    if (Enu < Emin)
-      return std::numeric_limits<double>::min();
-    else
-      return NeutrinoDISCrossSectionsFromTables::TotalCrossSection(Enu, flavor, neutype, current);
+  double LinearCrossSections::TotalCrossSection(double Enu, NeutrinoFlavor flavor,
+								       NeutrinoType neutype, Current current) const {
+    return 1.e-7*GF*GF*Enu*mp;
   }
-  
-  double NeutrinoDISCrossSectionsFromTablesExtended::SingleDifferentialCrossSection(double E1, double E2, NeutrinoFlavor flavor, NeutrinoType neutype, Current current) const{
-    // we assume that sterile neutrinos are trully sterile
-    if (not (flavor == electron or flavor == muon or flavor == tau))
+
+  double LinearCrossSections::SingleDifferentialCrossSection(double E1, double E2, NeutrinoFlavor flavor, NeutrinoType neutype, Current current) const {
+    if (E2 > E1)
       return 0.0;
-    if (E1 < Emin || E2<Emin)
-      return std::numeric_limits<double>::min();
+    else if (E2 < 0)
+      return 0.0;
     else
-      return NeutrinoDISCrossSectionsFromTables::SingleDifferentialCrossSection(E1, E2, flavor, neutype, current);
+      return (TotalCrossSection(E1,flavor,neutype,current)/E1)*(2.*E2/E1);
   }
-    
-}
+} // close nusquids namespace
 
--- examples/Xsections/exCross.h
+++ examples/Xsections/exCross.h
@@ -12,34 +12,26 @@
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_multimin.h>
-//#include <string>
 
 #include <SQuIDS/SQuIDS.h>
 #include <nuSQuIDS/nuSQuIDS.h>
 
+namespace nusquids {
 
-namespace nusquids{
+// LinearCrossSections
+// Simple toy cross section that scales linearly with energy and where
+// the differential cross section is proportional to the out-going
+// neutrino energy.
 
-
-// NeutrinoDISCrossSectionsFromTablesExtended
-// Is basically a copy of the default NeutrinoDISCrossSectionsFromTables but instead of 
-// returning error when the energy is lower that the low energy value in the tables it returns zero.
-// This is effectively true for the range of energies given in the nuSQuIDS default tables(Emin=1e2GeV)
-// This allows to use a wider range of the energy to compute atmospheric oscillations as it's shown in the 
-// main.cpp example.
-  
-  class NeutrinoDISCrossSectionsFromTablesExtended : public NeutrinoDISCrossSectionsFromTables {
+class LinearCrossSections : public NeutrinoCrossSections {
+  private:
+    const double GF = 1.16639e-23; // eV^-2
+    const double mp = 938.272e6; // proton mass eV
   public :
-    NeutrinoDISCrossSectionsFromTablesExtended():NeutrinoDISCrossSectionsFromTables(){}
+    LinearCrossSections(){}
     double TotalCrossSection(double Enu, NeutrinoFlavor flavor, NeutrinoType neutype, Current current) const override;
     double SingleDifferentialCrossSection(double E1, double E2, NeutrinoFlavor flavor, NeutrinoType neutype, Current current) const override;
   };
-  
-  
-  
-  
-}
-
-
+} // close nusquids namespace
 
 #endif
