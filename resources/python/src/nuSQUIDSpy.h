17d4a949d8e9c4325038fcafff5a054d2106ba82
C. Weaver <cnweaver@gmail.com>
2020-12-10 22:04:19 -0500

    Update python bindings to work with CrossSectionLibrary changes

--- resources/python/src/nuSQUIDSpy.h
+++ resources/python/src/nuSQUIDSpy.h
@@ -272,7 +272,7 @@ template<typename BaseType, typename = typename std::enable_if<std::is_base_of<n
       class_object->def(init<marray<double,1>,unsigned int>(args("E_vector","numneu")));
       class_object->def(init<marray<double,1>,unsigned int,NeutrinoType>(args("E_vector","numneu","NT")));
       class_object->def(init<marray<double,1>,unsigned int,NeutrinoType,bool>(args("E_vector","numneu","NT","iinteraction")));
-      class_object->def(init<marray<double,1>,unsigned int,NeutrinoType,bool,std::shared_ptr<NeutrinoCrossSections>>(args("E_vector","numneu","NT","iinteraction","ncs")));
+      class_object->def(init<marray<double,1>,unsigned int,NeutrinoType,bool,std::shared_ptr<CrossSectionLibrary>>(args("E_vector","numneu","NT","iinteraction","ncs")));
       class_object->def(init<std::string>(args("filename")));
       class_object->def(init<std::string, std::string>(args("filename","root group name")));
       class_object->def(init<std::string, std::string, std::shared_ptr<nusquids::nuSQUIDS::InteractionStructure>>(args("filename","root group name","interaction structure")));
@@ -361,7 +361,7 @@ template<typename BaseType, typename = typename std::enable_if<std::is_base_of<n
 
       class_object->def(init<marray<double,1>,marray<double,1>,unsigned int,NeutrinoType>(args("CosZenith_vector","E_vector","numneu","NT")));
       class_object->def(init<marray<double,1>,marray<double,1>,unsigned int,NeutrinoType,bool>(args("CosZenith_vector","E_vector","numneu","NT","iinteraction")));
-      class_object->def(init<marray<double,1>,marray<double,1>,unsigned int,NeutrinoType,bool,std::shared_ptr<NeutrinoCrossSections>>(args("CosZenith_vector","E_vector","numneu","NT","iinteraction","ncs")));
+      class_object->def(init<marray<double,1>,marray<double,1>,unsigned int,NeutrinoType,bool,std::shared_ptr<CrossSectionLibrary>>(args("CosZenith_vector","E_vector","numneu","NT","iinteraction","ncs")));
       class_object->def(init<std::string>(args("filename")));
       class_object->def("EvolveState",&nuSQUIDSAtm<BaseType>::EvolveState);
       class_object->def("Set_TauRegeneration",&nuSQUIDSAtm<BaseType>::Set_TauRegeneration);
