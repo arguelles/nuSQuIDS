ef85986059945ad5fca0d44e00c83443d9d18439
C. Weaver <cnweaver@gmail.com>
2020-12-30 14:41:03 -0500

    Tell gcc to trust us about throwing exceptions

--- include/nuSQuIDS/marray.h
+++ include/nuSQuIDS/marray.h
@@ -1349,6 +1349,8 @@ private:
 		allocator_traits::deallocate(alloc,buffer,size);
 	}
 	
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wterminate"
 	///Initialize a buffer by copy constructing objects from corresponding objects in another buffer
 	///
 	///May throw if value_type(const value_type&) throws
@@ -1370,6 +1372,7 @@ private:
 			throw;
 		}
 	}
+	#pragma GCC diagnostic pop
 };
 
 template<typename Iterator, int dummy=Iterator::marray_iterator_tag>
