ae6a14ec5f449d000d25b121d7fe94b0aa96f350
C. Weaver <cnweaver@gmail.com>
2021-02-14 11:39:04 -0500

    Make the external flux test work for matter

--- test/external_flux.test.cpp
+++ test/external_flux.test.cpp
@@ -1,12 +1,14 @@
-#include <nuSQuIDS/nuSQuIDS.h>
 #include <iostream>
 #include <iomanip>
 #include <vector>
 
+#include <nuSQuIDS/nuSQuIDS.h>
+#include <nuSQuIDS/resources.h>
+
 using namespace nusquids;
 
 // Exponential source
-// The assumption will be that it will be a single flavor neutrino source
+// The assumption will be that it will be a all flavor neutrino source
 // with an exponential decay profile.
 class EmittingSlab: public ConstantDensity{
   private:
@@ -19,7 +21,6 @@ class EmittingSlab: public ConstantDensity{
       for(unsigned int ei=0; ei < nusquids.GetNumE(); ei++){
         for(unsigned int rhoi = 0; rhoi < nusquids.GetNumRho(); rhoi++){
           for(unsigned int flv = 0; flv < nusquids.GetNumNeu(); flv++){
-            //flux[ei][rhoi][flv] = (flv == flavor) ? exp(-x_cur/decay_length) : 0.0;
             flux[ei][rhoi][flv] = exp(-x_cur/decay_length);
           }
         }
@@ -34,8 +35,12 @@ class EmittingSlab: public ConstantDensity{
 // lambda_1 : interaction_length
 // lambda_2 : decay_length
 double analytical_solution(double t, double interaction_length, double decay_length){
-  double c = 1./decay_length - 1./interaction_length;
-  return (1 - exp(-t*c))/c;
+  if(interaction_length>=std::numeric_limits<double>::max()){
+    //when lambda_1 -> infinity the phi(t) term can be ignored
+    return decay_length*(1-exp(-t/decay_length));
+  }
+  double c=(interaction_length*decay_length)/(decay_length-interaction_length);
+  return -c*exp(-t/interaction_length)+c*exp(-t/decay_length);
 }
 
 void test(const nuSQUIDS& nus,
@@ -48,31 +53,28 @@ void test(const nuSQUIDS& nus,
       double exact = analytical_solution(track->GetFinalX(),interaction_length,decay_length);
       double del = nus.EvalFlavor(fl, E) - exact;
       double rel = del/exact;
-      //std::cout << fl << " " << exact << " " << nus.EvalFlavor(fl, E) << " " << del << std::endl;
-      if(fabs(rel) > 1.e-5 or std::isnan(del))
-        std::cout << fl << " " << del << std::endl;
+      if(std::abs(rel) > 1.e-5 or std::isnan(del))
+        std::cout << fl << ' ' << del << '\n';
     }
   }
 }
 
 int main(){
   squids::Const units;
+  const double integration_tol=1e-8;
 
   {// interactions true and osc true, vac
+    std::cout << "Interactions true, oscillations true, vacuum" << std::endl;
     nuSQUIDS nus(logspace(1.e2*units.GeV,1.e6*units.GeV,60),3,neutrino,true);
     double decay_length = 100.0*units.meter;
-    //unsigned int flv = 1; // nu mu
     std::shared_ptr<EmittingSlab> emit = std::make_shared<EmittingSlab>(decay_length,0.0,0.5);
     std::shared_ptr<EmittingSlab::Track> track = std::make_shared<EmittingSlab::Track>(1.0*units.km);
     nus.Set_Body(emit);
     nus.Set_Track(track);
-    nus.Set_rel_error(1.0e-8);
-    nus.Set_abs_error(1.0e-8);
-    nus.Set_GSL_step(gsl_odeiv2_step_rk4);
+    nus.Set_rel_error(integration_tol);
+    nus.Set_abs_error(integration_tol);
     nus.Set_NeutrinoSources(true);
     nus.Set_IncludeOscillations(true);
-    //nus.Set_IncludeOscillations(false);
-    //nus.Set_ProgressBar(true);
 
     marray<double,1> E_range = nus.GetERange();
     marray<double,2> inistate{E_range.size(),nus.GetNumNeu()};
@@ -84,20 +86,17 @@ int main(){
   }
 
   {// interactions true and osc false, vac
+    std::cout << "Interactions true, oscillations false, vacuum" << std::endl;
     nuSQUIDS nus(logspace(1.e2*units.GeV,1.e6*units.GeV,60),3,neutrino,true);
     double decay_length = 100.0*units.meter;
-    //unsigned int flv = 1; // nu mu
     std::shared_ptr<EmittingSlab> emit = std::make_shared<EmittingSlab>(decay_length,0.0,0.5);
     std::shared_ptr<EmittingSlab::Track> track = std::make_shared<EmittingSlab::Track>(1.0*units.km);
     nus.Set_Body(emit);
     nus.Set_Track(track);
-    nus.Set_rel_error(1.0e-8);
-    nus.Set_abs_error(1.0e-8);
-    nus.Set_GSL_step(gsl_odeiv2_step_rk4);
+    nus.Set_rel_error(integration_tol);
+    nus.Set_abs_error(integration_tol);
     nus.Set_NeutrinoSources(true);
     nus.Set_IncludeOscillations(false);
-    //nus.Set_IncludeOscillations(false);
-    //nus.Set_ProgressBar(true);
 
     marray<double,1> E_range = nus.GetERange();
     marray<double,2> inistate{E_range.size(),nus.GetNumNeu()};
@@ -109,20 +108,17 @@ int main(){
   }
 
   {// interactions false  and osc false, vac
+    std::cout << "Interactions false, oscillations false, vacuum" << std::endl;
     nuSQUIDS nus(logspace(1.e2*units.GeV,1.e6*units.GeV,60),3,neutrino,false);
     double decay_length = 100.0*units.meter;
-    //unsigned int flv = 1; // nu mu
     std::shared_ptr<EmittingSlab> emit = std::make_shared<EmittingSlab>(decay_length,0.0,0.5);
     std::shared_ptr<EmittingSlab::Track> track = std::make_shared<EmittingSlab::Track>(1.0*units.km);
     nus.Set_Body(emit);
     nus.Set_Track(track);
-    nus.Set_rel_error(1.0e-8);
-    nus.Set_abs_error(1.0e-8);
-    nus.Set_GSL_step(gsl_odeiv2_step_rk4);
+    nus.Set_rel_error(integration_tol);
+    nus.Set_abs_error(integration_tol);
     nus.Set_NeutrinoSources(true);
     nus.Set_IncludeOscillations(false);
-    //nus.Set_IncludeOscillations(false);
-    //nus.Set_ProgressBar(true);
 
     marray<double,1> E_range = nus.GetERange();
     marray<double,2> inistate{E_range.size(),nus.GetNumNeu()};
@@ -134,20 +130,17 @@ int main(){
   }
 
   {// interactions false  and osc true, vac
+    std::cout << "Interactions false, oscillations true, vacuum" << std::endl;
     nuSQUIDS nus(logspace(1.e2*units.GeV,1.e6*units.GeV,60),3,neutrino,false);
     double decay_length = 100.0*units.meter;
-    //unsigned int flv = 1; // nu mu
     std::shared_ptr<EmittingSlab> emit = std::make_shared<EmittingSlab>(decay_length,0.0,0.5);
     std::shared_ptr<EmittingSlab::Track> track = std::make_shared<EmittingSlab::Track>(1.0*units.km);
     nus.Set_Body(emit);
     nus.Set_Track(track);
-    nus.Set_rel_error(1.0e-8);
-    nus.Set_abs_error(1.0e-8);
-    nus.Set_GSL_step(gsl_odeiv2_step_rk4);
+    nus.Set_rel_error(integration_tol);
+    nus.Set_abs_error(integration_tol);
     nus.Set_NeutrinoSources(true);
     nus.Set_IncludeOscillations(true);
-    //nus.Set_IncludeOscillations(false);
-    //nus.Set_ProgressBar(true);
 
     marray<double,1> E_range = nus.GetERange();
     marray<double,2> inistate{E_range.size(),nus.GetNumNeu()};
@@ -158,22 +151,48 @@ int main(){
     test(nus,1000,2,6,std::numeric_limits<double>::max(),decay_length,track);
   }
 
-  {// interactions true and osc true, matter
+  // We can't write down a convenient analytical solution to the differential 
+  // equation for the neutrino flux when NC cascading is taking effect, so to
+  // test that we do the right thing in the presence of absorption we use a 
+  // trick cross section which is only non-zero for charged current interactions
+  // (which purely remove neutrinos in our treatment)
+  class CCOnlyCrossSections : public nusquids::NeutrinoDISCrossSectionsFromTables{
+    using Parent=nusquids::NeutrinoDISCrossSectionsFromTables;
+  public:
+    CCOnlyCrossSections(std::string p):Parent(p){}
+    double TotalCrossSection(double Enu, NeutrinoFlavor flavor, NeutrinoType neutype, Current current) const override{
+      if(current!=CC)
+        return 0;
+      return Parent::TotalCrossSection(Enu, flavor, neutype, current);
+    }
+    double SingleDifferentialCrossSection(double E1, double E2, NeutrinoFlavor flavor, NeutrinoType neutype, Current current) const override{
+      if(current!=CC)
+        return 0;
+      return Parent::SingleDifferentialCrossSection(E1, E2, flavor, neutype, current);
+    }
+  };
+  std::shared_ptr<CrossSectionLibrary> ccOnlyXS(new CrossSectionLibrary);
+  {
+    std::string xsdir = nusquids::getResourcePath()+"/xsections/";
+    ccOnlyXS->addTarget(nusquids::proton, CCOnlyCrossSections(xsdir+"csms_proton.h5"));
+    ccOnlyXS->addTarget(nusquids::neutron,CCOnlyCrossSections(xsdir+"csms_neutron.h5"));
+  }
+  
+  {// interactions true and osc false, matter
+    std::cout << "Interactions true, oscillations false, matter" << std::endl;
     nuSQUIDS nus(logspace(1.e2*units.GeV,1.e6*units.GeV,60),3,neutrino,true);
+    nus.SetNeutrinoCrossSections(ccOnlyXS);
     double decay_length = 100.0*units.meter;
-    //unsigned int flv = 1; // nu muo
     double density = 5.0;
+    //use a material which has no neutrons to reduce the number of cross sections we need to deal with
     std::shared_ptr<EmittingSlab> emit = std::make_shared<EmittingSlab>(decay_length,density,1.0);
-    std::shared_ptr<EmittingSlab::Track> track = std::make_shared<EmittingSlab::Track>(100.0*units.km);
+    std::shared_ptr<EmittingSlab::Track> track = std::make_shared<EmittingSlab::Track>(100*units.km);
     nus.Set_Body(emit);
     nus.Set_Track(track);
-    nus.Set_rel_error(1.0e-10);
-    nus.Set_abs_error(1.0e-10);
-    nus.Set_GSL_step(gsl_odeiv2_step_rk4);
+    nus.Set_rel_error(integration_tol);
+    nus.Set_abs_error(integration_tol);
     nus.Set_NeutrinoSources(true);
     nus.Set_IncludeOscillations(false);
-    //nus.Set_IncludeOscillations(false);
-    //nus.Set_ProgressBar(true);
 
     marray<double,1> E_range = nus.GetERange();
     marray<double,2> inistate{E_range.size(),nus.GetNumNeu()};
@@ -181,26 +200,62 @@ int main(){
     nus.Set_initial_state(inistate,flavor);
     nus.EvolveState();
 
-    // computing/getting the interaction length
     auto int_struct = nus.GetInteractionStructure();
-    double target_number_density = 2.*density*units.gr*pow(units.cm,-3)/(units.proton_mass + units.neutron_mass + units.electron_mass);
-    /*
-    std::cout << int_struct->sigma_NC.extent(0) << std::endl;
-    std::cout << int_struct->sigma_NC[0][0][0][10] << " " << int_struct->sigma_NC[1][0][0][10] << std::endl;
-    std::cout << int_struct->sigma_CC[0][0][0][10] << " " << int_struct->sigma_CC[1][0][0][10] << std::endl;
-    return 0;
-    */
+    double target_number_density = density*units.gr*pow(units.cm,-3)/(units.proton_mass + units.electron_mass);
+    
     for(unsigned int ie = 0; ie < E_range.size(); ie++){
       double E=E_range[ie];
       for(unsigned int fl=0; fl<nus.GetNumNeu(); fl++){
-        double interaction_length = 1./((int_struct->sigma_NC[0][0][fl][ie] + int_struct->sigma_CC[0][0][fl][ie])*target_number_density);
-        //interaction_length = std::numeric_limits<double>::max();
+        //we used only proton targets and CC cross sections
+        double invlen = int_struct->sigma_CC[0][0][fl][ie]*target_number_density;
+        double interaction_length = 1./invlen;
         double exact = analytical_solution(track->GetFinalX(),interaction_length,decay_length);
-        double del = nus.EvalFlavor(fl, E) - exact;
+        double computed = nus.EvalFlavor(fl, E); 
+        double del = computed - exact;
         double rel = del/exact;
-        //std::cout << fl << " " << exact << " " << nus.EvalFlavor(fl, E) << " " << del << std::endl;
-        if(fabs(rel) > 1.e-5 or std::isnan(del))
-          std::cout << fl << " " << del <<  " " << rel << std::endl;
+        if(std::abs(rel) > 1.e-5 or std::isnan(del))
+          std::cout << ie << ' ' << fl << ' ' << del <<  ' ' << rel << ' ' << exact << ' ' << computed << '\n';
+      }
+    }
+  }
+  
+  {// interactions true and osc true, matter
+    std::cout << "Interactions true, oscillations true, matter" << std::endl;
+    nuSQUIDS nus(logspace(1.e2*units.GeV,1.e6*units.GeV,60),3,neutrino,true);
+    nus.SetNeutrinoCrossSections(ccOnlyXS);
+    double decay_length = 100.0*units.meter;
+    double density = 5.0;
+    //use a material which has no neutrons to reduce the number of cross sections we need to deal with
+    std::shared_ptr<EmittingSlab> emit = std::make_shared<EmittingSlab>(decay_length,density,1.0);
+    std::shared_ptr<EmittingSlab::Track> track = std::make_shared<EmittingSlab::Track>(100*units.km);
+    nus.Set_Body(emit);
+    nus.Set_Track(track);
+    nus.Set_rel_error(integration_tol);
+    nus.Set_abs_error(integration_tol);
+    nus.Set_NeutrinoSources(true);
+    nus.Set_IncludeOscillations(true);
+
+    marray<double,1> E_range = nus.GetERange();
+    marray<double,2> inistate{E_range.size(),nus.GetNumNeu()};
+    std::fill(inistate.begin(),inistate.end(),0.0);
+    nus.Set_initial_state(inistate,flavor);
+    nus.EvolveState();
+
+    auto int_struct = nus.GetInteractionStructure();
+    double target_number_density = density*units.gr*pow(units.cm,-3)/(units.proton_mass + units.electron_mass);
+    
+    for(unsigned int ie = 0; ie < E_range.size(); ie++){
+      double E=E_range[ie];
+      for(unsigned int fl=0; fl<nus.GetNumNeu(); fl++){
+        //we used only proton targets and CC cross sections
+        double invlen = int_struct->sigma_CC[0][0][fl][ie]*target_number_density;
+        double interaction_length = 1./invlen;
+        double exact = analytical_solution(track->GetFinalX(),interaction_length,decay_length);
+        double computed = nus.EvalFlavor(fl, E); 
+        double del = computed - exact;
+        double rel = del/exact;
+        if(std::abs(rel) > 1.e-5 or std::isnan(del))
+          std::cout << ie << ' ' << fl << ' ' << del <<  ' ' << rel << ' ' << exact << ' ' << computed << '\n';
       }
     }
   }
