360180afc1955763145c58b7ecdff19ccaec55e5
Lukas Hennig <lukas.hennig@fau.de>
2025-01-24 16:07:08 +0100

    first draft for nuSQUIDSLVAtm

--- include/nuSQuIDS/nuSQuIDSLV.h
+++ include/nuSQuIDS/nuSQuIDSLV.h
@@ -44,6 +44,53 @@ class nuSQUIDSLV: public nuSQUIDS {
     void Set_initial_state(const marray<double, 3> &ini_state, Basis basis = flavor);
 };
 
+class nuSQUIDSLVAtm : public nuSQUIDSAtm<nuSQUIDSLV> {
+
+  public:
+
+    // Use the base class constructors
+    using nuSQUIDSAtm<nuSQUIDSLV>::nuSQUIDSAtm;
+
+    // nuSQUIDSLVAtm has an attribute nusq_array that contains one nuSQUIDSLV object
+    // for each zenith. This array can be obtained with this->GetnuSQuIDS(). The Setter
+    // methods have to be wrapped such that the Setter of each element of that array is called
+
+    void Set_LV_OpMatrix(LVParameters &lv_params){
+      for(nuSQUIDSLV& nsq : this->GetnuSQuIDS()) nsq.Set_LV_OpMatrix(lv_params);
+    }
+
+    void Set_LV_OpMatrix(gsl_matrix_complex *cmatrix){
+      for(nuSQUIDSLV& nsq : this->GetnuSQuIDS()) nsq.Set_LV_OpMatrix(cmatrix);
+    }
+
+    void Set_LV_Operator(squids::SU_vector op){
+      for(nuSQUIDSLV& nsq : this->GetnuSQuIDS()) nsq.Set_LV_Operator(op);
+    }
+
+    void Set_LV_EnergyPower(int n){
+      for(nuSQUIDSLV& nsq : this->GetnuSQuIDS()) nsq.Set_LV_EnergyPower(n);
+    }
+
+    void Set_MixingAngle(unsigned int i, unsigned int j, double angle){
+      for(nuSQUIDSLV& nsq : this->GetnuSQuIDS()) nsq.Set_MixingAngle(i, j, angle);
+    }
+
+    void Set_CPPhase(unsigned int i, unsigned int j, double angle){
+      for(nuSQUIDSLV& nsq : this->GetnuSQuIDS()) nsq.Set_CPPhase(i, j, angle);
+    }
+
+    void Set_initial_state(const marray<double, 1> &ini_state, Basis basis = flavor){
+      for(nuSQUIDSLV& nsq : this->GetnuSQuIDS()) nsq.Set_initial_state(ini_state, basis);
+    }
+
+    void Set_initial_state(const marray<double, 2> &ini_state, Basis basis = flavor){
+      for(nuSQUIDSLV& nsq : this->GetnuSQuIDS()) nsq.Set_initial_state(ini_state, basis);
+    }
+
+    void Set_initial_state(const marray<double, 3> &ini_state, Basis basis = flavor){
+      for(nuSQUIDSLV& nsq : this->GetnuSQuIDS()) nsq.Set_initial_state(ini_state, basis);
+    }
+}; 
 } // namespace nusquids
 
 #endif // nusquidslv_H
